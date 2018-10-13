07457030793d23b6fb5767cb8b52c29db8a66801
Bernard12 <ivnsimakhin@gmail.com>
2018-10-14 00:13:25 +0300

    Small refactor

--- src/main/java/com/github/sh0nk/matplotlib4j/builder/SubplotBuilderImpl.java
+++ src/main/java/com/github/sh0nk/matplotlib4j/builder/SubplotBuilderImpl.java
@@ -1,8 +1,7 @@
 package com.github.sh0nk.matplotlib4j.builder;
 
 public class SubplotBuilderImpl implements SubplotBuilder {
-    private final CompositeBuilder<SubplotBuilder> innerBuilder =
-            new CompositeBuilder<>(this);
+    private final CompositeBuilder<SubplotBuilder> innerBuilder = new CompositeBuilder<>(this);
 
     public SubplotBuilderImpl(int nrows, int ncols, int index) {
         innerBuilder.addToArgs(nrows);
